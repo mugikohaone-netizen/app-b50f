942fc614a2f0bd4d674a9355eca432a7989bd2f9
mugikohaone-netizen <mugikoha.one@gmail.com>
2026-07-20 06:29:46 +0900

    🔄 SW更新 vmrsb6x69

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-mrs93dzj';
+const CACHE = 'app-mrsb6x69';
 const FILES = ['./','./index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
