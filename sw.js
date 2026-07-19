617519d06205f47612a7ee00942219fd628c70d9
mugikohaone-netizen <mugikoha.one@gmail.com>
2026-07-19 21:32:14 +0900

    🔄 SW更新 vmrrrzn2s

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-mrrrdkav';
+const CACHE = 'app-mrrrzn2s';
 const FILES = ['./','./index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
